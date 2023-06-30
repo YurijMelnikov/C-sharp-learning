e9d72eeec49da13d65040a047039db662f310cc7
Melnikov Yurij <voolkan85@mail.ru>
2023-06-30 14:01:58 +0300

    Home work 6

---
+++ Home_work/06/task_01/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+double[] FilingArray(int arrayLength)
+{
+    double[] array = new double[arrayLength];
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"Введите {i + 1} число: ");
+        array[i] = Convert.ToDouble(Console.ReadLine());
+    }
+    return array;
+}
+
+int PositiveCount(double[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) counter++;
+    }
+    return counter;
+}
+
+System.Console.Write("Сколько чисел будет введено? ");
+int length = Convert.ToInt32(Console.ReadLine());
+double[] array = FilingArray(length);
+System.Console.WriteLine($"Количество введённых чисел больше нуля: {PositiveCount(array)}");
---
+++ Home_work/06/task_02/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+double VariableCreation(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double IntersectionPPoint(double k1, double b1, double k2, double b2)
+{
+    double intersectionPPointX = (b2 - b1) / (k1 - k2);
+    return intersectionPPointX;
+}
+System.Console.WriteLine("Заданы две прямых y = k1 * x + b1 и y = k2 * x + b2: найдите точку пересечения этих прямых");
+double k1 = VariableCreation("Введите k1: ");
+double b1 = VariableCreation("Введите b1: ");
+double k2 = VariableCreation("Введите k2: ");
+double b2 = VariableCreation("Введите b2: ");
+if (k1 == k2 && b1 == b2) System.Console.WriteLine("Прямые идентичны");
+else if (k1 == k2 && b1 != b2) System.Console.WriteLine("Прямые параллельны");
+else System.Console.WriteLine($"Точка пересечения двух прямых: y = {k1} * x + {b1} и y = {k2} * x + {b2}: -> ({Math.Round(IntersectionPPoint(k1, b1, k2, b2), 2)}; {Math.Round(k1 * IntersectionPPoint(k1, b1, k2, b2) + b1, 2)})");
